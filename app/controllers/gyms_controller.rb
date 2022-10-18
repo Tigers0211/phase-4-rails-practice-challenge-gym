24400d810912bd02bb34f849352dd755c1b64b15
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-18 11:06:36 -0400

    show and destroy

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,2 +1,19 @@
 class GymsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def show
+        gym = Gym.find(params[:id])
+        render json: gym, status: :ok
+    end
+
+    def destroy
+       Gym.find(params[:id]).destroy
+       head :no_content
+    end
+
+    private
+
+    def not_found
+        render json: {error: "Gym not found"}, status: :not_found
+    end
 end
