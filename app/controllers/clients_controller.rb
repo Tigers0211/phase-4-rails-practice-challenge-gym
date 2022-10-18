5064230c42e6e6f032ccc2edea600f7527363fee
Caleb <Tigers1025@gmail.com>
2022-10-18 11:22:52 -0400

    Commit

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,15 @@
 class ClientsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    
+    def show
+        client = Client.find(params[:id])
+       render json: client, status: :ok
+       
+    end
+
+    private
+
+    def not_found
+        render json: {error: "Client not found"}, status: :not_found
+    end
 end
